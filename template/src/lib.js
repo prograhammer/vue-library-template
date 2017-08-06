1a818c6b2ec67553a6572a9e8768cc5a8f1b03aa
David Graham <prograhammer@gmail.com>
2017-08-06 09:22:59 -0500

    add lib styles

--- template/src/lib.js
+++ template/src/lib.js
@@ -1,3 +1,9 @@
 import {{ name }} from './components/hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{#sass}}
+import './styles/lib.scss'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/sass}}
+{{#stylus}}
+import './styles/lib.styl'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/stylus}}
 
 export default {{ name }}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
